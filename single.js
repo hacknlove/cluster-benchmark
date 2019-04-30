00447c6f2e002b5b91545ab4ed2acaf2d03d5905
antonio <antonio@pykiss.com>
2019-04-30 13:09:15 +0200

    log start

--- single.js
+++ single.js
@@ -9,3 +9,5 @@ controller(server)
 
 server.server = http.createServer(server.app)
 server.server.listen(process.env.PORT || 8080, process.env.IP || '127.0.0.1')
+
+console.log('escuchando en ', process.env.PORT || 8080, process.env.IP || '127.0.0.1')
